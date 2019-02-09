9412d8794ad14c0fdb7c1fe556cad8130bad6171
gokulrv4399 <41222104+gokulrv4399@users.noreply.github.com>
2019-02-09 02:20:36 -0800

    Create set16.c

---
+++ set16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a;
+    clrscr();
+    
+    scanf("%d",&a);
+    if(a%4==0||a%400==0&& a%100==0) 
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    
+    getch();
+}
