45396bc4896333f8468574300296c438eae118bf
gokulrv4399 <41222104+gokulrv4399@users.noreply.github.com>
2019-02-09 01:54:03 -0800

    Create set12.c

---
+++ set12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a;
+    clrscr();
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("Even");
+        
+    }
+    else
+    {
+     printf("Odd");   
+    }
+    getch();
+}
