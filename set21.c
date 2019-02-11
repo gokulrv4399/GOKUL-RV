f8b2b50307a9f25613e1b5bd94c44493f147dd0b
gokulrv4399 <41222104+gokulrv4399@users.noreply.github.com>
2019-02-11 02:24:12 -0800

    Create set21.c

---
+++ set21.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,k,count=0;
+    clrscr();
+    scanf("%d",&n);
+    scanf("%d",&k);
+    count=pow(n,k);
+    
+    printf("%d",count);
+  getch();
+}
