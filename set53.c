19af0c4feda254065deca05a5ea9878396d7c9a7
gokulrv4399 <41222104+gokulrv4399@users.noreply.github.com>
2019-02-16 02:29:18 -0800

    Update set53.c

--- set53.c
+++ set53.c
@@ -2,17 +2,17 @@
 #include<conio.h>
 void main()
 {
-char s[20],s2[20];
+char s11[20],s2[20];
 int i,j;
 clrscr();
-gets(s);
+gets(s11);
 gets(s2);
-for(i=0;s[i]!='\0';++i);
+for(i=0;s11[i]!='\0';++i);
 for(j=0;s2[j]!='\0';++j,++i)
 {
-s[i]=s2[j];
+s11[i]=s2[j];
 }
-s[i]='\0';
-printf("%s",s);
+s11[i]='\0';
+printf("%s",s11);
 getch();
 }
