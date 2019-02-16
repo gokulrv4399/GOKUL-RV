5c78941509157db2167d8ae3cb2702f8dfc6dc30
gokulrv4399 <41222104+gokulrv4399@users.noreply.github.com>
2019-02-16 02:34:04 -0800

    Update set55.c

--- set55.c
+++ set55.c
@@ -2,12 +2,12 @@
 #include<conio.h>
 void main()
 {
-int n1,count=0;
+int n11,count=0;
 clrscr();
-scanf("%d",&n1);
-while(n1)
+scanf("%d",&n11);
+while(n11)
 {
-n1=n1/10;
+n11=n11/10;
 count++;
 }
 printf("%d",count);
