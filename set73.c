bf0afe1df3716b2cbe3b78a8a8d396ab12c29378
gokulrv4399 <41222104+gokulrv4399@users.noreply.github.com>
2019-02-20 02:15:51 -0800

    Create set73.c

---
+++ set73.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n=10,min,a1[20],i;
+clrscr();
+for(i=0;i<n;i++)
+{
+scanf("%d",&a1[i]);
+}
+min=a[0];
+for(i=0;i<n;i++)
+{
+if(a1[i]<min)
+{
+min=a1[i];
+}
+}
+printf("%d",min);
+getch();
+}
