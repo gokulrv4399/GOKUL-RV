6c4f883d18e9392899f5263b5c19cd43c69f9b24
gokulrv4399 <41222104+gokulrv4399@users.noreply.github.com>
2019-02-20 02:07:56 -0800

    Create set68.c

---
+++ set68.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,k1,a[30],des=0,i;
+clrscr();
+scanf("%d%d",&n1,&k1);
+for(i=0;i<n1;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n1;i++)
+{
+if(a[i]==k1)
+{
+printf("yes");
+des=0;
+break;
+}
+else
+{
+des=1;
+}
+}
+if(des==1)
+{
+printf("no");
+}
+getch();
+}
